5200439734085a35d4bc4186a1730d45ae83cff5
Laura Denney <laura.a.denney@gmail.com>
2021-03-08 22:41:12 -0800

    working on day5 part 1

---
+++ adventday5/main.py
@@ -0,0 +1,43 @@
+input = []
+with open('input.txt','r') as fr:
+  input = fr.read()
+
+lst_tickets = input.split("\n")
+
+lst_ids = []
+
+def get_id(row, column):
+    id = (row*8) + column
+    return id
+
+def process_ticket(ticket):
+    binary_row = ticket[:7]
+    binary_column = ticket[7:]
+    row = find_row(binary_row)
+    column = find_column(binary_column)
+    return row, column
+
+def find_row(br):
+    rows = list(range(128))
+    for item in br:
+        if item == "B":
+            rows = rows[int(len(rows/2)):]
+        else:
+            rows = rows[:int(len(rows)/2)]
+    return rows
+
+def find_column(bc):
+    columns = list(range(7))
+    for item in bc:
+        if item == "R":
+            columns = columns[int(len(columns/2)):]
+        else:
+            columns = columns[:int(len(columns)/2)]
+    return columns
+
+for ticket in lst_tickets:
+    row, column = process_ticket(ticket)
+    id = get_id(row, column)
+    lst_ids.append(id)
+
+print(max(lst_ids))
