01b64d0596816f5dd8d3a327987a41170176043a
Laura Denney <laura.a.denney@gmail.com>
2021-03-08 22:56:24 -0800

    finished day5 code

--- adventday5/main.py
+++ adventday5/main.py
@@ -21,23 +21,31 @@ def find_row(br):
     rows = list(range(128))
     for item in br:
         if item == "B":
-            rows = rows[int(len(rows/2)):]
+            rows = rows[int(len(rows)/2):]
         else:
             rows = rows[:int(len(rows)/2)]
     return rows
 
 def find_column(bc):
-    columns = list(range(7))
+    columns = list(range(8))
     for item in bc:
         if item == "R":
-            columns = columns[int(len(columns/2)):]
+            columns = columns[int(len(columns)/2):]
         else:
             columns = columns[:int(len(columns)/2)]
     return columns
 
 for ticket in lst_tickets:
     row, column = process_ticket(ticket)
-    id = get_id(row, column)
+    id = get_id(row[0], column[0])
     lst_ids.append(id)
 
 print(max(lst_ids))
+
+lst_ids.sort()
+counter = 0
+for x in range(89,989):
+    if lst_ids[counter]== x:
+        counter+=1
+    else:
+        print(x)
