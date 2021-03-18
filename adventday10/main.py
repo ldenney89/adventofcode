cc8df420fc4ee429657c86f44be95fc259485c28
Laura Denney <laura.a.denney@gmail.com>
2021-03-17 23:00:06 -0700

    completed part 1

--- adventday10/main.py
+++ adventday10/main.py
@@ -7,14 +7,22 @@ lst_input = input.split("\n")
 
 nums = [int(x) for x in lst_input]
 
+nums.append(0)
 nums.sort()
+nums.append(nums[-1]+3)
 
-count1=0
-count3=0
-for x in range(len(nums)-2):
-  diff = nums[x+1] - nums[x]
-  if diff == 1:
-    count1+=1
-  elif diff == 3:
-    count3+=1
-print(count1*count3)
+
+def countdiffs(nums):
+  count1=0
+  count3=0
+  for x in range(0,len(nums)-1):
+    diff = nums[x+1] - nums[x]
+    if diff == 1:
+      count1+=1
+    elif diff == 3:
+      count3+=1
+    else:
+      raise Exception
+  print(count1*count3)
+
+
